00d08df4b4bc615b8be5c04544884ff57061aa20
cto_varun <cto_varun@gmail.com>
2017-09-08 17:29:30 +0530

    new changes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  root "app_pages#home"
   get 'tutorials/bookList'
 
   get 'tutorials/pageList'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ 
+ image = Image.create([{description: 'default'},{size: '100'}])
+ 
+ News.create([{image_id: image.id},{content: 'first news'}])
+ 
+ subject = Subject.create([{title: 'new subject'}])
+ 
+ klass = Klass.create([{title: 'new class'}])
+ 
+ board = Board.create([{short_name: 'new'},{state:'new state'},{full_name:'new full name'}])
+ 
+ Course.create([{course_name:'foundation'},{subject_id: subject.id },{klass_id: klass.id},{board_id: board.id},{fee:'1000'},{duration:'1'},{description: 'new course'}])
+ 
+ category1 = Category.create([{category_name: 'admin'}])
+ 
+ category2 = Category.create([{category_name: 'center'}])
+ 
+ User.create([{user_name: 'amitshr532@gmail.com'},{email: 'amitshr532@gmail.com'},{password: 'adminamit'},{category_id: category1.id},{email_verification: '1'},{status: '1'}])
+  
+ user = User.create([{user_name: 'newcenter@gmail.com'},{email: 'newcenter@gmail.com'},{password: 'center123'},{category_id: category1.id},{email_verification: '1'},{status: '1'}])
+ 
+ Center.create([{user_id: user.id},{center_name:'new center'},{center_incharge:'amit sharma'},{mobile_number:'9872569970'}])
